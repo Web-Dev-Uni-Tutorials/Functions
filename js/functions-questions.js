c75955117467e45d7a43a7dc93d1b63901cd9ee0
M Mantle <mmantlehud@googlemail.com>
2020-11-21 13:50:13 +0000

    Initial commit

--- js/functions-questions.js
+++ js/functions-questions.js
@@ -6,11 +6,34 @@ c) Add additional parameters for the continent, capital city and population of t
 d) Make several function calls to print out the details of different countries
 */
 
-// function printCountryDetails(country, continent, capital, population)
-// {
-//     console.log(`${country} is in ${continent}. It has a capital city of ${capital} and a population of ${population}`);
-// }
-// printCountryDetails("England","Europe","London","53 million");
+/*
+//a
+function printCountryDetails()
+{
+	console.log("England is in Europe. It has a capital city of London and a population of 53 million");
+}
+printCountryDetails();
+*/
+
+/*
+//b
+function printCountryDetails(name)
+{
+	console.log(`${name} is in Europe. It has a capital city of London and a population of 53 million`);
+}
+printCountryDetails("England");
+*/
+
+/*
+//c and d
+function printCountryDetails(country, continent, capital, population)
+{
+    console.log(`${country} is in ${continent}. It has a capital city of ${capital} and a population of ${population}`);
+}
+printCountryDetails("England","Europe","London","53 million");
+printCountryDetails("France","Europe","Paris","67 million");
+printCountryDetails("Japan","Asia","Tokyo","120 million");
+*/
 
 /*
 2. Look at the following function, changePage().
@@ -19,15 +42,33 @@ b) Add a parameter to this function that will specify the colour the background 
 c) Add a second parameter that will allow the user to specify the font for the text in the page. To change the font you will need to use document.body.style.fontFamily.(slides 7-10)
 */
 
+/*
+//a
+function changePage()
+{
+ document.body.style.backgroundColor="red";
+}
+changePage();
+*/
 
-function changePage(bgCol,font,txtCol)
+/*
+//b
+function changePage(bgCol)
+{
+ document.body.style.backgroundColor=bgCol;
+}
+changePage("pink");
+*/
+
+/*
+//c
+function changePage(bgCol,font)
 {
  document.body.style.backgroundColor=bgCol;
  document.body.style.fontFamily=font;
- document.body.style.color=txtCol;
 }
-changePage("red","Arial","blue");
-
+changePage("red","Arial");
+*/
 
 
 /*
@@ -35,15 +76,18 @@ changePage("red","Arial","blue");
 Write a line of code that will call this function and display the returned value in the console (slide 11)
 */
 
+/*
 function getWeather(){
     return "Wet and cold";
 }
 console.log(getWeather())
+*/
 
 /*
 4. The following function converts a measurement in miles to kilometres (1 mile = 1.60934km). Write a program that will ask the user for a distance in miles, call this function to convert the miles to kilometres, and then display the distance in kilometres in the console. (slide 13)
 */
 
+/*
 function convertMilesToKm(miles)
 {
 	return miles*1.60934
@@ -51,7 +95,7 @@ function convertMilesToKm(miles)
 const userMiles = parseInt(prompt("Enter a number of miles?"));
 const kms = convertMilesToKm(userMiles);
 console.log(`${userMiles} miles is ${kms}kms`);
-
+*/
 
 /*
 5. At the end of your degree course, the University needs to decide if it will award you a degree.
@@ -60,6 +104,7 @@ Write a program that will ask the user how many credits they have.
 The program should call awardDegree() and feedback to the user telling them if they can be awarded a degree. (slide 16)
 */
 
+/*
 function awardDegree(credits)
 {
 	if(credits<360)
@@ -74,23 +119,27 @@ if(awardDegree(userCredits)){
 }else{
     console.log("Sorry, not enough credits.")
 }
+*/
+
 /*
 6. Modify your answer to Question 1 so that the function accepts a single argument. This argument should be a country with continent, capital city and population properties. The function should print the country's details. The following shows example code that would call this function. (slide 17)
 */
 
-// function printCountryDetails(country)
-// {
-//     console.log(`${country.name} is in ${country.continent}. It has a capital city of ${country.capital} and a population of ${country.population}.`);
-// }
-//
-// const country = {name : "England", capital : "London", continent : "Europe", population: 53000000};
-// printCountryDetails(country);
+/*
+function printCountryDetails(country)
+{
+    console.log(`${country.name} is in ${country.continent}. It has a capital city of ${country.capital} and a population of ${country.population}.`);
+}
+
+const country = {name : "England", capital : "London", continent : "Europe", population: 53000000};
+printCountryDetails(country);
+*/
 
 /*
 7. Modify your answer to Question 1 again so that the function accepts a single argument. This time the  argument should be an array of country objects. The following shows example code that would call this function. (slide 21)
 */
 
-
+/*
 function printCountryDetails(countries)
 {
 	countries.forEach(function(country){
@@ -105,11 +154,18 @@ const countries=[
 ]
 
 printCountryDetails(countries);
+*/
+
+/*
+These are harder questions. You can see them as being optional. You only need to attempt them if you want more practice and want to challenge yourself.
+*/
 
 
 /*
 8. Write a function called calcAverage(). The calcAverage() function should accept three numbers as arguments. The function should calculates the average of the three numbers return the mean average of these numbers.The following shows example code that would call this function.
 */
+
+/*
 function calcAverage(num1,num2,num3)
 {
     const total = (num1+num2+num3)/3;
@@ -118,6 +174,7 @@ function calcAverage(num1,num2,num3)
 
 const ave = calcAverage(25,40,25);
 console.log(`The average is ${ave}`); //30
+*/
 
 /*
 9. Look at the following function calls.
@@ -126,6 +183,8 @@ These arguments represent the length of the sides of a triangle.
 The function should work out if the triangle is an equilateral triangle (all sides the same length), an isosceles triangle (two sides of the same length), or a scaline triangle (all sides different lengths. The function should return a string either ‘equilateral’, ‘scalene’ or ‘isosceles’. Write the typeOfTriangle function and test it
 works.
 */
+
+/*
 function typeOfTriangle(side1,side2,side3)
 {
     if(side1===side2 && side1===side3){
@@ -138,6 +197,7 @@ function typeOfTriangle(side1,side2,side3)
 console.log(typeOfTriangle(2,4,2)); //should output isoceles
 console.log(typeOfTriangle(2,2,2)); //should output equilateral
 console.log(typeOfTriangle(1,2,3)); //should output scalene
+*/
 
 /*
 10. Uncomment the following code. Write some additional code that will:
@@ -155,6 +215,7 @@ function getNewStudent()
   const newStudent = {firstName: firstName, lastName: lastName, mark: mark};
   return newStudent;
 }
+
 function addStudents()
 {
 	const students=[];
@@ -169,6 +230,7 @@ function getPassMark(){
 	const passMark = parseInt(prompt("What is the pass mark?"));
 	return passMark;
 }
+
 function getPassingStudents(arrOfStudents,passMark)
 {
 	const passingStudents = arrOfStudents.filter(function(student){
@@ -179,11 +241,14 @@ function getPassingStudents(arrOfStudents,passMark)
 	});
 	return passingStudents;
 }
-function printStudents(arrOfStudents){
+
+function printStudents(arrOfStudents)
+{
 	arrOfStudents.forEach(function(student){
 		console.log(`${student.firstName} ${student.lastName} : ${student.mark}`)
 	})
 }
+
 function getHighestMarkStudent(arrOfStudents)
 {
 	const highestMarkStudent = arrOfStudents.reduce(function(curr,next){
@@ -194,30 +259,37 @@ function getHighestMarkStudent(arrOfStudents)
 	});
 	return highestMarkStudent;
 }
+
+/*
 const passMark = getPassMark();
 const students = addStudents()
 const passingStudents = getPassingStudents(students,passMark);
 printStudents(passingStudents);
 const highestStudent = getHighestMarkStudent(students);
 console.log(`The student with the highest mark is ${highestStudent.firstName} ${highestStudent.lastName}.`)
-
+*/
 
 /*
 11. Previously we looked at a program that calculates an employee's wage based on the number of hours they have worked.Re-structure this code to use a number of functions - getUserHours(), validHours(), calcWages() and printWages().Here's some example code to show how these functions could be used
 */
-function getUserHours(){
+
+/*
+function getUserHours()
+{
     const hours=parseInt(prompt("Enter the number of hours you have worked"));
     return hours;
 }
 
-function validHours(hours){
+function validHours(hours)
+{
     if(isNaN(hours) || hours<1 || hours>80){
         return false
     }
     return true;
 }
 
-function calcWages(hours){
+function calcWages(hours)
+{
     let wages;
     if(hours>40){
         const overtimeHours = hours-40;
@@ -240,3 +312,4 @@ if(validHours(hours)){
 }else{
     console.log("You need to enter a number between 1 and 80");
 }
+*/
