81e0a8f174ec8647894bfc6f193a4272d2b0b786
mmantle-hud <m.e.mantle@hud.ac.uk>
2021-09-14 12:17:35 +0100

    Update functions-questions.js

--- js/functions-questions.js
+++ js/functions-questions.js
@@ -1,11 +1,5 @@
 /*
-1.Look at the following function printCountryDetails().
-a) Write a line of code that will call this function, (slide 4)
-b) Add a parameter to the function, call the parameter name. When the function is called an argument should specify the name of the country e.g. printCountryDetails("England"); (slide 7)
-c) Add additional parameters for the continent, capital city and population of the country. (slides 7-10)
-d) Make several function calls to print out the details of different countries
-*/
-
+1.
 /*
 //a
 function printCountryDetails()
@@ -36,10 +30,7 @@ printCountryDetails("Japan","Asia","Tokyo","120 million");
 */
 
 /*
-2. Look at the following function, changePage().
-a) Add line of code that will call the function changePage. The page should change colour to red.(slide 4)
-b) Add a parameter to this function that will specify the colour the background should change to.(slide 7)
-c) Add a second parameter that will allow the user to specify the font for the text in the page. To change the font you will need to use document.body.style.fontFamily.(slides 7-10)
+2.
 */
 
 /*
@@ -72,8 +63,7 @@ changePage("red","Arial");
 
 
 /*
-3. Look at the following function getWeather(), it returns a string.
-Write a line of code that will call this function and display the returned value in the console (slide 11)
+3.
 */
 
 /*
@@ -84,7 +74,7 @@ console.log(getWeather())
 */
 
 /*
-4. The following function converts a measurement in miles to kilometres (1 mile = 1.60934km). Write a program that will ask the user for a distance in miles, call this function to convert the miles to kilometres, and then display the distance in kilometres in the console. (slide 13)
+4.
 */
 
 /*
@@ -98,10 +88,7 @@ console.log(`${userMiles} miles is ${kms}kms`);
 */
 
 /*
-5. At the end of your degree course, the University needs to decide if it will award you a degree.
-In order to obtain a degree you need to have accumulated 360 credits.
-Write a program that will ask the user how many credits they have.
-The program should call awardDegree() and feedback to the user telling them if they can be awarded a degree. (slide 16)
+5. 
 */
 
 /*
@@ -122,7 +109,7 @@ if(awardDegree(userCredits)){
 */
 
 /*
-6. Modify your answer to Question 1 so that the function accepts a single argument. This argument should be a country with continent, capital city and population properties. The function should print the country's details. The following shows example code that would call this function. (slide 17)
+6.
 */
 
 /*
@@ -136,7 +123,7 @@ printCountryDetails(country);
 */
 
 /*
-7. Modify your answer to Question 1 again so that the function accepts a single argument. This time the  argument should be an array of country objects. The following shows example code that would call this function. (slide 21)
+7.
 */
 
 /*
@@ -156,13 +143,11 @@ const countries=[
 printCountryDetails(countries);
 */
 
-/*
-These are harder questions. You can see them as being optional. You only need to attempt them if you want more practice and want to challenge yourself.
-*/
+
 
 
 /*
-8. Write a function called calcAverage(). The calcAverage() function should accept three numbers as arguments. The function should calculates the average of the three numbers return the mean average of these numbers.The following shows example code that would call this function.
+8. 
 */
 
 /*
@@ -177,11 +162,7 @@ console.log(`The average is ${ave}`); //30
 */
 
 /*
-9. Look at the following function calls.
-These lines of code call a function and pass three argument.
-These arguments represent the length of the sides of a triangle.
-The function should work out if the triangle is an equilateral triangle (all sides the same length), an isosceles triangle (two sides of the same length), or a scaline triangle (all sides different lengths. The function should return a string either ‘equilateral’, ‘scalene’ or ‘isosceles’. Write the typeOfTriangle function and test it
-works.
+9. 
 */
 
 /*
@@ -200,13 +181,10 @@ console.log(typeOfTriangle(1,2,3)); //should output scalene
 */
 
 /*
-10. Uncomment the following code. Write some additional code that will:
-a) Call the functions addStudents, getPassMark and getPassingStudents, and then print out the list of passing students in the console.
-b) Create an additional function called printStudents. printStudents will need to accept a single argument, an array of student objects. The function will then print the name of each student in this array to the console. The function doesn't need to return anything. Modify the answer to (a) so that you use the printStudents function.
-c) Create an additional function called getHighestMarkStudent. getHighestMarkStudent will need to accept a single argument, an array of student objects. The function should then return the student with the highest mark.
+10. 
 */
 
-
+/*
 function getNewStudent()
 {
   const firstName = prompt("Enter a first name");
@@ -218,7 +196,7 @@ function getNewStudent()
 
 function addStudents()
 {
-	const students=[];
+  const students=[];
   const howMany=parseInt(prompt("How many students details would you like to enter?"));
   for(let i=0;i<howMany;i++){
       const student = getNewStudent();
@@ -227,37 +205,37 @@ function addStudents()
   return students;
 }
 function getPassMark(){
-	const passMark = parseInt(prompt("What is the pass mark?"));
-	return passMark;
+  const passMark = parseInt(prompt("What is the pass mark?"));
+  return passMark;
 }
 
 function getPassingStudents(arrOfStudents,passMark)
 {
-	const passingStudents = arrOfStudents.filter(function(student){
-		if(student.mark>passMark){
-			return true;
-		}
-		return false;
-	});
-	return passingStudents;
+    const passingStudents = arrOfStudents.filter(function(student){
+	if(student.mark>passMark){
+		return true;
+	}
+        return false;
+    });
+    return passingStudents;
 }
 
 function printStudents(arrOfStudents)
 {
-	arrOfStudents.forEach(function(student){
-		console.log(`${student.firstName} ${student.lastName} : ${student.mark}`)
-	})
+     arrOfStudents.forEach(function(student){
+         console.log(`${student.firstName} ${student.lastName} : ${student.mark}`)
+     })
 }
 
 function getHighestMarkStudent(arrOfStudents)
 {
-	const highestMarkStudent = arrOfStudents.reduce(function(curr,next){
-		if(curr.mark>next.mark){
-			return curr;
-		}
-		return next;
-	});
-	return highestMarkStudent;
+    const highestMarkStudent = arrOfStudents.reduce(function(curr,next){
+	if(curr.mark>next.mark){
+		return curr;
+	}
+	return next;
+    });
+    return highestMarkStudent;
 }
 
 /*
@@ -270,7 +248,7 @@ console.log(`The student with the highest mark is ${highestStudent.firstName} ${
 */
 
 /*
-11. Previously we looked at a program that calculates an employee's wage based on the number of hours they have worked.Re-structure this code to use a number of functions - getUserHours(), validHours(), calcWages() and printWages().Here's some example code to show how these functions could be used
+11.
 */
 
 /*
