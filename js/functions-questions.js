fce561dd00f0636f84fb133504f6fcffde9d4f94
mmantle-hud <m.e.mantle@hud.ac.uk>
2021-11-18 16:33:38 +0000

    Update functions-questions.js

--- js/functions-questions.js
+++ js/functions-questions.js
@@ -1,80 +1,50 @@
+
+//These first questions are all about calling (or using) functions that have already been written
+
 /*
-1.
-/*
-//a
-function printCountryDetails()
+1.Look at the following function showMsg().
+Write a line of code that will call this function so that the message 'functions are great' is displayed in the console.
+*/
+
+
+function showMsg()
 {
-	console.log("England is in Europe. It has a capital city of London and a population of 53 million");
+	console.log("functions are great");
 }
-printCountryDetails();
+
+showMsg();
+
+
+/*
+2. Look at the following function showMsgWithParam().
+Write a line of code that will call this function so that the message 'functions are great' is displayed in the console.
 */
 
 /*
-//b
-function printCountryDetails(name)
+function showMsgWithParam(msg)
 {
-	console.log(`${name} is in Europe. It has a capital city of London and a population of 53 million`);
+	console.log(msg);
 }
-printCountryDetails("England");
+
+showMsgWithParam('functions are great');
 */
 
 /*
-//c and d
-function printCountryDetails(country, continent, capital, population)
-{
-    console.log(`${country} is in ${continent}. It has a capital city of ${capital} and a population of ${population}`);
-}
-printCountryDetails("England","Europe","London","53 million");
-printCountryDetails("France","Europe","Paris","67 million");
-printCountryDetails("Japan","Asia","Tokyo","120 million");
-*/
-
-/*
-2.
-*/
-
-/*
-//a
-function changePage()
-{
- document.body.style.backgroundColor="red";
-}
-changePage();
-*/
-
-/*
-//b
-function changePage(bgCol)
-{
- document.body.style.backgroundColor=bgCol;
-}
-changePage("pink");
-*/
-
-/*
-//c
-function changePage(bgCol,font)
-{
- document.body.style.backgroundColor=bgCol;
- document.body.style.fontFamily=font;
-}
-changePage("red","Arial");
-*/
-
-
-/*
-3.
+3. Look at the following function getWeather(), it returns a string.
+Write a line of code that will call this function and display the returned value in the console
 */
 
 /*
 function getWeather(){
-    return "Wet and cold";
+	return "wet and cold";
 }
-console.log(getWeather())
+const weather = getWeather();
+console.log(`The weather is ${weather}.`);
 */
 
 /*
-4.
+4. The following function converts a measurement in miles to kilometres (1 mile = 1.60934km).
+Write a program that will ask the user for a distance in miles, call this function, and then display the distance in kilometres in the console.
 */
 
 /*
@@ -82,87 +52,343 @@ function convertMilesToKm(miles)
 {
 	return miles*1.60934
 }
-const userMiles = parseInt(prompt("Enter a number of miles?"));
-const kms = convertMilesToKm(userMiles);
-console.log(`${userMiles} miles is ${kms}kms`);
+
+const miles = parseInt(prompt("Enter a measurement in miles"));
+const kms = convertMilesToKm(miles);
+console.log(`${miles} miles is equivalent to ${kms} kms`);
+*/
+
+
+/*
+3) The following function, addTwoNumbers(), adds two numbers together and returns the result.
+Write a program that will ask the user for two numbers, call this function, and then display the total in the console.
 */
 
 /*
-5. 
+function addTwoNumbers(num1,num2)
+{
+	return num1 + num2;
+}
+const num1 = parseInt(prompt("Enter a number"));
+const num2 = parseInt(prompt("Enter another number"));
+const total = addTwoNumbers(num1, num2);
+console.log(`The total of ${num1} and ${num2} is ${total}.`);
+*/
+
+/*
+4. At the end of your degree course, the University needs to decide if it will award you a degree.
+In order to obtain a degree you need to have accumulated 360 credits.
+Write a program that will ask the user how many credits they have.
+The program should call awardDegree() and feedback to the user telling them if they can be awarded a degree.
 */
 
 /*
 function awardDegree(credits)
 {
-	if(credits<360)
- 	{
+	if(credits < 360){
  		return false;
  	}
  	return true;
 }
-const userCredits = prompt("How many credits do you have?")
-if(awardDegree(userCredits)){
-    console.log("Well done.")
+const credits = parseInt(prompt("How many credits do you have?"));
+if(awardDegree(credits)){
+	console.log(`Congratulations, you can be awarded a degree.`);
 }else{
-    console.log("Sorry, not enough credits.")
+	console.log(`Sorry, not enough credits.`);
 }
 */
 
 /*
-6.
+5) Look at the following getPositiveNumbers() function. It accepts an array of numbers and returns only those numbers that are greater than zero.
+a) Write a some code that will call this function. Use a forEach loop output the elements of the returned array.
+*/
+
+/*
+function getPositiveNumbers(arrOfNumbers)
+{
+	const posNumbers = arrOfNumbers.filter(function(num){
+		if(num > 0){
+			return true;
+		}
+		return false;
+	})
+	return posNumbers;
+}
+
+const nums = [32,54329876,-23,4,-8271,478];
+const posNumbers = getPositiveNumbers(nums);
+posNumbers.forEach(function(num){
+	console.log(num);
+})
+*/
+
+/*
+6.Look at the following function printCountryDetails().
+a) Write a line of code that will call this function.
+*/
+
+/*
+function printCountryDetails()
+{
+	console.log(`England is a country in Europe.`);
+}
+printCountryDetails();
+*/
+
+/*
+b) Add a parameter to the function, call the parameter name.
+Use the parameter in the console.log() message. When the function is called an argument should specify the name of the country.
+To test your function, uncomment the following lines.
 */
 
 /*
 function printCountryDetails(country)
 {
-    console.log(`${country.name} is in ${country.continent}. It has a capital city of ${country.capital} and a population of ${country.population}.`);
+	console.log(`${country} is a country in Europe.`);
 }
 
-const country = {name : "England", capital : "London", continent : "Europe", population: 53000000};
-printCountryDetails(country);
+printCountryDetails("France"); //outputs 'England is a country in Europe.'
+printCountryDetails("England") //outputs 'France is a country in Europe.'
 */
 
 /*
-7.
+c) Add additional parameters for the continent, capital city and population of the country.
+To test your function, uncomment the following lines.
+*/
+
+/*
+function printCountryDetails(country, continent, capital, population)
+{
+	console.log(`${country} is a country in ${continent} with a population of ${population}. ${capital} is the capital city.`);
+}
+printCountryDetails("England", "Europe", "London", 56000000); //outputs England is a country in Europe with a population of 56000000. London is the capital city.
+printCountryDetails("France", "Europe", "Paris", 65000000); //outputs France is a country in Europe with a population of 65000000. Paris is the capital city.
+*/
+
+/*
+7. Modify your answer to Question 6 so that the function accepts a single argument.
+This argument should be a country object with continent, capital city and population properties.
+The function should print the country's details.
+To test your function, uncomment the following lines.
+*/
+
+/*
+function printCountryDetails(country)
+{
+	console.log(`${country.name} is a country in ${country.continent} with a population of ${country.population}. ${country.capital} is the capital city.`);
+}
+const country = {name : "England", capital : "London", continent : "Europe", population: 56000000};
+printCountryDetails(country); //outputs 'England is a country in Europe with a population of 56000000. London is the capital city.'
+*/
+
+/*
+8. Modify your answer to Question 7 so that the function accepts a single argument.
+This time the argument should be an array of country objects.
+To test your function, uncomment the following lines.
 */
 
 /*
 function printCountryDetails(countries)
 {
 	countries.forEach(function(country){
-		console.log(`${country.name} is in ${country.continent}. It has a capital city of ${country.capital} and a population of ${country.population}.`);
+		console.log(`${country.name} is a country in ${country.continent} with a population of ${country.population}. ${country.capital} is the capital city.`);
 	})
 }
 
 const countries=[
-    {name : "England", capital : "London", continent : "Europe", population: 53000000},
-    {name : "France", capital : "Paris", continent : "Europe", population: 67000000},
+    {name : "England", capital : "London", continent : "Europe", population: 56000000},
+    {name : "France", capital : "Paris", continent : "Europe", population: 65000000},
     {name : "USA", capital : "Washington", continent : "N. America", population: 325000000}
-]
+];
 
-printCountryDetails(countries);
-*/
-
-
-
-
-/*
-8. 
+printCountryDetails(countries); //displays the details for each country in the console.
 */
 
 /*
-function calcAverage(num1,num2,num3)
+9. Write a function called convertInchesToCms().
+The function needs to accept a single argument, a measurement in inches.
+The function should convert the measurement in inches into centimetres (1 inch = 2.54 cms) and return the result.
+To test your function, uncomment the following lines.
+*/
+
+/*
+function convertInchesToCms(inches)
 {
-    const total = (num1+num2+num3)/3;
-    return total;
+	return inches * 2.54;
 }
 
-const ave = calcAverage(25,40,25);
-console.log(`The average is ${ave}`); //30
+const inches = prompt("Enter a number of inches");
+const cms = convertInchesToCms(inches);
+console.log(`${inches} inches is ${cms} cms.`)
+*/
+
+
+/*
+10. Write a function called getAvgThreeNums().
+The function needs to accept a three arguments, all numbers.
+The function should calculate the mean average of the numbers and return the result.
+To test your function, uncomment the following lines.
 */
 
 /*
-9. 
+function getAvgThreeNums()
+{
+	const avg = (num1 + num2 + num3) / 3;
+	return avg;
+}
+
+const num1 = parseInt(prompt("Enter a number"));
+const num2 = parseInt(prompt("Enter another number"));
+const num3 = parseInt(prompt("Enter a third number"));
+const avg = getAvgThreeNums(num1,num2,num3);
+console.log(`The average of ${num1}, ${num2} and ${num3} is ${avg}.`)
+*/
+
+/*
+11. Write a function called filterImageFileNames().
+It should accept an array of filename and returns only those filenames that have a .png or .jpg extension.
+To test your function, uncomment the following lines.
+*/
+
+/*
+function filterImageFileNames(fileNames)
+{
+	const imageFileNames = fileNames.filter(function(fileName){
+		if(fileName.indexOf(".png") > -1 || fileName.indexOf(".jpg") > -1){
+			return true;
+		}
+		return false;
+	})
+	return imageFileNames;
+}
+
+const fileNames = ["report.doc", "kitten.png", "pen.jpg", "index.html", "mug.png"];
+const imageFileNames = filterImageFileNames(fileNames);
+
+imageFileNames.forEach(function(fileName){
+	console.log(fileName);
+})
+*/
+
+
+/*
+12. Uncomment the following code. Write some additional code that will:
+a) Call the functions addStudents(), getPassMark() and getPassingStudents(), and then print out the list of passing students in the console.
+b) Create an additional function called printStudents(). printStudents() will need to accept a single argument, an array of student objects.
+The function will then print the name of each student in this array to the console.
+The function doesn't need to return anything. Modify the answer to (a) so that you use the printStudents() function.
+c) Create an additional function called getHighestMarkStudent().
+getHighestMarkStudent() will need to accept a single argument, an array of student objects.
+The function should then return the student with the highest mark. Test this works.
+*/
+
+/*
+function getNewStudent()
+{
+  const firstName = prompt("Enter a first name");
+  const lastName = prompt("Enter a last name");
+  const mark = parseInt(prompt(`What mark did ${firstName} ${lastName} get?`));
+  const newStudent = {firstName: firstName, lastName: lastName, mark: mark};
+  return newStudent;
+}
+function addStudents()
+{
+  const students=[];
+  const howMany=parseInt(prompt("How many students details would you like to enter?"));
+  for(let i = 0; i < howMany; i++){
+      const student = getNewStudent();
+      students.push(student);
+  }
+  return students;
+}
+function getPassMark(){
+  const passMark = parseInt(prompt("What is the pass mark?"));
+  return passMark;
+}
+function getPassingStudents(arrOfStudents,passMark)
+{
+  const passingStudents = arrOfStudents.filter(function(student){
+	  if(student.mark > passMark){
+		  return true;
+	  }
+    return false;
+  });
+  return passingStudents;
+}
+function printStudents(arrOfStudents)
+{
+     arrOfStudents.forEach(function(student){
+         console.log(`${student.firstName} ${student.lastName} : ${student.mark}`)
+     })
+}
+function getHighestMarkStudent(arrOfStudents)
+{
+  const highestMarkStudent = arrOfStudents.reduce(function(curr,next){
+	  if(curr.mark > next.mark){
+		  return curr;
+	  }
+	  return next;
+  });
+  return highestMarkStudent;
+}
+
+const passMark = getPassMark();
+const students = addStudents()
+const passingStudents = getPassingStudents(students,passMark);
+printStudents(passingStudents);
+const highestStudent = getHighestMarkStudent(students);
+console.log(`The student with the highest mark is ${highestStudent.firstName} ${highestStudent.lastName}.`)
+*/
+
+/*
+13. Previously we looked at a program that calculates an employee's wage based on the number of hours they have worked.
+Re-structure this code to use a number of functions - getUserHours(), validHours(), calcWages() and printWages().
+Here's some example code to show how these functions could be used
+*/
+
+/*
+function getUserHours()
+{
+    const hours=parseInt(prompt("Enter the number of hours you have worked"));
+    return hours;
+}
+function validHours(hours)
+{
+    if(isNaN(hours) || hours<1 || hours>80){
+        return false
+    }
+    return true;
+}
+function calcWages(hours)
+{
+    let wages;
+    if(hours>40){
+        const overtimeHours = hours-40;
+        wages = overtimeHours*3 + 40*2
+    }else{
+        wages = hours*2
+    }
+    return wages;
+}
+function printWages(hours,wages)
+{
+    console.log(`You worked ${hours} hours. You earnt £${wages}.`);
+}
+const hours = getUserHours();
+if(validHours(hours)){
+    const wages = calcWages(hours);
+    printWages(hours, wages);
+}else{
+    console.log("You need to enter a number between 1 and 80");
+}
+*/
+
+/*
+14.  Write a function called typeOfTriangle().
+It should accept three arguments that represent the length of the three sides of a triangle.
+The function should work out if the triangle is an equilateral triangle (all sides the same length), an isosceles triangle (two sides of the same length), or a scaline triangle (all sides different lengths.
+The function should return a string, either 'equilateral', 'scalene' or 'isosceles'.
+To test your function, uncomment the following lines.
 */
 
 /*
@@ -178,116 +404,4 @@ function typeOfTriangle(side1,side2,side3)
 console.log(typeOfTriangle(2,4,2)); //should output isoceles
 console.log(typeOfTriangle(2,2,2)); //should output equilateral
 console.log(typeOfTriangle(1,2,3)); //should output scalene
-*/
-
-/*
-10. 
-*/
-
-/*
-function getNewStudent()
-{
-  const firstName = prompt("Enter a first name");
-  const lastName = prompt("Enter a last name");
-  const mark = parseInt(prompt(`What mark did ${firstName} ${lastName} get?`));
-  const newStudent = {firstName: firstName, lastName: lastName, mark: mark};
-  return newStudent;
-}
-
-function addStudents()
-{
-  const students=[];
-  const howMany=parseInt(prompt("How many students details would you like to enter?"));
-  for(let i=0;i<howMany;i++){
-      const student = getNewStudent();
-      students.push(student);
-  }
-  return students;
-}
-function getPassMark(){
-  const passMark = parseInt(prompt("What is the pass mark?"));
-  return passMark;
-}
-
-function getPassingStudents(arrOfStudents,passMark)
-{
-    const passingStudents = arrOfStudents.filter(function(student){
-	if(student.mark>passMark){
-		return true;
-	}
-        return false;
-    });
-    return passingStudents;
-}
-
-function printStudents(arrOfStudents)
-{
-     arrOfStudents.forEach(function(student){
-         console.log(`${student.firstName} ${student.lastName} : ${student.mark}`)
-     })
-}
-
-function getHighestMarkStudent(arrOfStudents)
-{
-    const highestMarkStudent = arrOfStudents.reduce(function(curr,next){
-	if(curr.mark>next.mark){
-		return curr;
-	}
-	return next;
-    });
-    return highestMarkStudent;
-}
-
-/*
-const passMark = getPassMark();
-const students = addStudents()
-const passingStudents = getPassingStudents(students,passMark);
-printStudents(passingStudents);
-const highestStudent = getHighestMarkStudent(students);
-console.log(`The student with the highest mark is ${highestStudent.firstName} ${highestStudent.lastName}.`)
-*/
-
-/*
-11.
-*/
-
-/*
-function getUserHours()
-{
-    const hours=parseInt(prompt("Enter the number of hours you have worked"));
-    return hours;
-}
-
-function validHours(hours)
-{
-    if(isNaN(hours) || hours<1 || hours>80){
-        return false
-    }
-    return true;
-}
-
-function calcWages(hours)
-{
-    let wages;
-    if(hours>40){
-        const overtimeHours = hours-40;
-        wages = overtimeHours*3 + 40*2
-    }else{
-        wages = hours*2
-    }
-    return wages;
-}
-
-function printWages(hours,wages)
-{
-    console.log(`You worked ${hours} hours. You earnt £${wages}.`);
-}
-
-const hours = getUserHours();
-if(validHours(hours)){
-    const wages = calcWages(hours);
-    printWages(hours, wages);
-}else{
-    console.log("You need to enter a number between 1 and 80");
-}
 */
